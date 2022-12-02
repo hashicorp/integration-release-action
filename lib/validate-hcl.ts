bfd581932ab92f41212d00f6246717456835f122
Kevin Wang <kwangsan@gmail.com>
2022-12-02 18:35:21 -0500

    set failed

--- lib/validate-hcl.ts
+++ lib/validate-hcl.ts
@@ -15,17 +15,22 @@ export async function main() {
   const releaseVersion = core.getInput("release_version", { required: true });
 
   // by default, assume the repo will be checked out under $GITHUB_WORKSPACE
-  let repoPath = core.getInput("repo_path") || "";
+  let repoPath = core.getInput("repo_path", {}) || "";
   repoPath = path.join(process.env.GITHUB_WORKSPACE!, repoPath);
 
   // exporting this ENV VAR because LoadFilesystemIntegration internally depends on this.
   process.env.INPUT_INTEGRATIONS_API_BASE_URL = getApiBaseUrl();
-  const fsIntegration = await LoadFilesystemIntegration({
-    identifier: integrationIdentifier,
-    repo_path: repoPath,
-    version: releaseVersion,
-  });
+  try {
+    const fsIntegration = await LoadFilesystemIntegration({
+      identifier: integrationIdentifier,
+      repo_path: repoPath,
+      version: releaseVersion,
+    });
 
-  core.info(JSON.stringify(fsIntegration, null, 2));
+    core.info(JSON.stringify(fsIntegration, null, 2));
+  } catch (err: any) {
+    core.setFailed(`[validate-hcl] Error: ${err.message}`);
+    throw err;
+  }
   core.info("[validate-hcl]: Finished");
 }
