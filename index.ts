780749236caaeec15f7f85cbc4fe61297a51f2fc
Kevin Wang <kwangsan@gmail.com>
2022-12-02 18:44:50 -0500

    don't throw

--- index.ts
+++ index.ts
@@ -2,7 +2,14 @@ import { main as notifyRelease } from "./lib/notify-release";
 import { main as validateHcl } from "./lib/validate-hcl";
 
 async function run() {
-  await validateHcl();
+  try {
+    await validateHcl();
+  } catch (err) {
+    // don't throw this error to avoid having GitHub actions
+    // print the minified dist/index.js code
+    return;
+  }
+
   await notifyRelease();
 }
 
